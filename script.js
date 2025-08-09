c20a27a5fe5e4ae36ef9cbb53e2baa97f66f761d
anakhapdileepkumar <anakhapdileep20@gmail.com>
2025-08-09 16:58:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let display = document.getElementById('display');
+
+function append(val) {
+  display.value += val;
+}
+function clearDisplay() {
+  display.value = '';
+}
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = 'Error';
+  }
+}
